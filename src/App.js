cac1b2feb4b0967c9ba5838c12107e6443c43c3b
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-24 17:54:52 -0600

    Add buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
 
 function App() {
+  const [runningCount, setRunningCount] = React.useState(0)
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Card Counting App</h1>
       </header>
+      <h3>Running Count: {runningCount}</h3>
+
+      <button onClick={() => setRunningCount(prevCount => prevCount+1)}>3</button>
+      <button onClick={() => setRunningCount(prevCount => prevCount+1)}>2</button>
+      <button onClick={() => setRunningCount(prevCount => prevCount+1)}>4</button>
+      <button onClick={() => setRunningCount(prevCount => prevCount+1)}>5</button>
+      <button onClick={() => setRunningCount(prevCount => prevCount+1)}>6</button>
+
+      <button onClick={() => setRunningCount(prevCount => prevCount-1)}>10</button>
+      <button onClick={() => setRunningCount(prevCount => prevCount-1)}>J</button>
+      <button onClick={() => setRunningCount(prevCount => prevCount-1)}>A</button>
+      <button onClick={() => setRunningCount(prevCount => prevCount-1)}>K</button>
+      <button onClick={() => setRunningCount(prevCount => prevCount-1)}>A</button>
     </div>
   );
 }

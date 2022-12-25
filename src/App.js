7a277a1f1397710fa00058c49e6ab2ae33175e9e
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-25 13:17:27 -0600

    Create changeNumber function and delete betting units state

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import Form from "./components/Form";
 
 function App() {
 
-  const [cardsRemaining, setCardsRemaining] = React.useState(104) //need
+  const [cardsRemaining, setCardsRemaining] = React.useState(104) 
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Card Counting App</h1>
       </header>
-      <Form setCardsRemaining={setCardsRemaining}/>
+      <Form setCardsRemaining={setCardsRemaining} cardsRemaining={cardsRemaining} bettingUnits={bettingUnits} setBettingUnits={setBettingUnits}/>
       <Game cardsRemaining={cardsRemaining} setCardsRemaining={setCardsRemaining} />
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,27 @@
 import React from 'react'
 
-export default function Form() {
+export default function Form({cardsRemaining, setCardsRemaining, bettingUnits, setBettingUnits}) {
+
+  const changeNumber = (e) => {
+    console.log(e.target.value)
+    setCardsRemaining(e.target.value * 52)
+  }
+
+  const changeBettingUnits = (e) => {
+    console.log(e.target.value)
+  }
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
   return (
     <div className='form-page'>
         <h2>Welcome to the Card Counter! Lets  make some money!!!</h2>
-        <form>
-            <label for="decks">Decks</label>
-            <input type="number" name="number" placeholder='2' />
-            <label for="bettingUnits">Betting Units</label>
-            <input type="number" name="bettingUnits" placeholder='25' />
-            <button>Submit</button>
+        <form onSubmit={onSubmit}>
+            <label htmlFor="decks">Decks</label>
+            <input type="number" name="number" placeholder={cardsRemaining/52} onChange={(e)=> changeNumber(e) } />
+            <label htmlFor="bettingUnits">Betting Units</label>
+            <input type="number" name="bettingUnits" placeholder={bettingUnits} onChange={(e)=> changeBettingUnits(e)}/>
+            <button type="submit" onClick={() => console.log(cardsRemaining)}>Submit</button>
         </form>
     </div>
   )

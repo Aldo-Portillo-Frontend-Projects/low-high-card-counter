27401f1c2babd671e0ca46cb46b1c6022f9cca2d
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-25 13:34:33 -0600

    Create deckNumber and gameRunninng State

--- src/App.js
+++ src/App.js
@@ -5,14 +5,19 @@ import Form from "./components/Form";
 function App() {
 
   const [cardsRemaining, setCardsRemaining] = React.useState(104) 
+  const [gameRunning, setGameRunning] = React.useState(false)
+  const [deckNumber, setDeckNumber] = React.useState(cardsRemaining/52)
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Card Counting App</h1>
       </header>
-      <Form setCardsRemaining={setCardsRemaining} cardsRemaining={cardsRemaining} bettingUnits={bettingUnits} setBettingUnits={setBettingUnits}/>
-      <Game cardsRemaining={cardsRemaining} setCardsRemaining={setCardsRemaining} />
+      {gameRunning ? 
+      <Game cardsRemaining={cardsRemaining} setCardsRemaining={setCardsRemaining} setGameRunning={setGameRunning} deckNumber={deckNumber} /> :
+      <Form setCardsRemaining={setCardsRemaining} cardsRemaining={cardsRemaining} setGameRunning={setGameRunning} setDeckNumber={setDeckNumber} deckNumber={deckNumber}/>}
+      
+      
     </div>
   );
 }

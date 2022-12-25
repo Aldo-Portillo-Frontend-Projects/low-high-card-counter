b49fe8634ca1bae528764133f79a457c1198f2cb
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-25 01:26:30 -0600

    Add cards remaining, true count and rounded deck

--- src/App.js
+++ src/App.js
@@ -2,6 +2,31 @@ import React from "react";
 
 function App() {
   const [runningCount, setRunningCount] = React.useState(0)
+  const [cardsRemaining, setCardsRemaining] = React.useState(52)
+  const [trueCount, setTrueCount] = React.useState(0)
+  const [roundedDeck, setRoundedDeck] = React.useState(cardsRemaining/52)
+
+  const deck = 52;
+
+  const drawLowCard = () => {
+    setRunningCount(prevCount => prevCount + 1)
+    setCardsRemaining(prevCount => prevCount -1)
+  }
+
+  
+
+  const drawHighCard = () => {
+    setRunningCount(prevCount => prevCount - 1)
+    setCardsRemaining(prevCount => prevCount -1)
+  }
+
+  function roundToHalf(num) {
+    return Math.round(num*2)/2;
+  } 
+  //Use effect to round deck
+  React.useEffect(() => {
+    //console.log(Math.round(cardsRemaining/52) *.5)
+  }, [cardsRemaining])
 
   return (
     <div className="App">
@@ -9,18 +34,21 @@ function App() {
         <h1>Card Counting App</h1>
       </header>
       <h3>Running Count: {runningCount}</h3>
+      <h3>Cards Remaining: {cardsRemaining}</h3>
+      <h3>True Count: {trueCount}</h3>
+      <h3>Rounded Deck: {roundedDeck}</h3>
 
-      <button onClick={() => setRunningCount(prevCount => prevCount+1)}>3</button>
-      <button onClick={() => setRunningCount(prevCount => prevCount+1)}>2</button>
-      <button onClick={() => setRunningCount(prevCount => prevCount+1)}>4</button>
-      <button onClick={() => setRunningCount(prevCount => prevCount+1)}>5</button>
-      <button onClick={() => setRunningCount(prevCount => prevCount+1)}>6</button>
+      <button onClick={() => drawLowCard()}>2</button>
+      <button onClick={() => drawLowCard()}>3</button>
+      <button onClick={() => drawLowCard()}>4</button>
+      <button onClick={() => drawLowCard()}>5</button>
+      <button onClick={() => drawLowCard()}>6</button>
 
-      <button onClick={() => setRunningCount(prevCount => prevCount-1)}>10</button>
-      <button onClick={() => setRunningCount(prevCount => prevCount-1)}>J</button>
-      <button onClick={() => setRunningCount(prevCount => prevCount-1)}>A</button>
-      <button onClick={() => setRunningCount(prevCount => prevCount-1)}>K</button>
-      <button onClick={() => setRunningCount(prevCount => prevCount-1)}>A</button>
+      <button onClick={() => drawHighCard()}>10</button>
+      <button onClick={() => drawHighCard()}>J</button>
+      <button onClick={() => drawHighCard()}>A</button>
+      <button onClick={() => drawHighCard()}>K</button>
+      <button onClick={() => drawHighCard()}>A</button>
     </div>
   );
 }

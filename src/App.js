f6ea5c7a2fcde97636cbf643bd41e20a73e0a705
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-25 12:52:28 -0600

    Pass set cards remaining down to Form

--- src/App.js
+++ src/App.js
@@ -5,19 +5,13 @@ import Form from "./components/Form";
 function App() {
 
   const [cardsRemaining, setCardsRemaining] = React.useState(104) //need
-  
-
-  
-
-  
-
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Card Counting App</h1>
       </header>
-      <Form />
+      <Form setCardsRemaining={setCardsRemaining}/>
       <Game cardsRemaining={cardsRemaining} setCardsRemaining={setCardsRemaining} />
     </div>
   );
